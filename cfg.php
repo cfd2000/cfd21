4e61611288c68361d5a349d6498ae732f3c339f3
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-07-17 14:18:07 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+#MASUKAN EMAIL PASSWORD & DATA MILIK KALIAN!!!
+$email = "dennykresno39@gmail.com";
+$password = "123456Qw";
+$Authorization = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjZTdhYzliNWM0ZDU0YzdmOWU3MWVkM2U5YTczMmMxMiIsImV4cCI6MTU5NDk3Mzc2Niwic3ViIjoiMTI4NDAwODk1MDczOTUwNTE1MiJ9.Yklzy2ZVA5W93cdJyZiG44pHpxNHED8XAgEjzJG1t1uOOBuj3Wr_B_ueang4Z1kOg7-IKnqHvJmxEk0HTiLr3Q";
+$deviceid = "8a15960fb2706e2391ef4a0bc4c70dae";
+$clientid = "ce7ac9b5c4d54c7f9e71ed3e9a732c12";
+$eagleeye_sessionid = "2f59790915949686427541048e";
+$eagleeye_pappname = "gmuvrygq39@cc48cab21253c7d";
+$eagleeye_traceid = "2d8dbe341594968643138100153c7d";
+$fingerprint = "e7b97cf070e333d26b08c27a1baf567a";
